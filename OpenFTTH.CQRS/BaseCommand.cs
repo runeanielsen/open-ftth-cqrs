41d14321813109f60b4c2194734bae623060a44d
Jesper Ladegaard <jesper@dax.dk>
2021-04-29 08:48:00 +0200

    BaseCommand added

---
+++ OpenFTTH.CQRS/BaseCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenFTTH.CQRS
+{
+    public abstract record BaseCommand
+    {
+        public Guid? CmdId { get; init; }
+        public DateTime Timestamp { get; init; }
+        public Guid? IncitingEventId { get; init; }
+        public string? UserName { get; init; }
+        public Guid? WorkTaskId { get; init; }
+    }
+}
