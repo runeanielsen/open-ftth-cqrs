3b9957b8d3fa51a961c12291a249c5463d9ab3a7
Jesper Ladegaard <jesper@dax.dk>
2021-06-14 20:06:42 +0200

    force parsing correlation id and user context to all commands

--- OpenFTTH.CQRS/BaseCommand.cs
+++ OpenFTTH.CQRS/BaseCommand.cs
@@ -4,9 +4,18 @@ namespace OpenFTTH.CQRS
 {
     public abstract record BaseCommand
     {
-        public Guid CmdId { get; init; }
-        public Guid CorrelationId { get; init; }
-        public DateTime Timestamp { get; init; }
-        public UserContext UserContext { get; init; }
+        public Guid CmdId { get;  }
+        public Guid CorrelationId { get; }
+        public DateTime Timestamp { get;  }
+        public UserContext UserContext { get; }
+
+        protected BaseCommand(Guid correlationId, UserContext userContext)
+        {
+            CmdId = Guid.NewGuid();
+            Timestamp = DateTime.UtcNow;
+
+            CorrelationId = correlationId;
+            UserContext = userContext;
+        }
     }
 }
